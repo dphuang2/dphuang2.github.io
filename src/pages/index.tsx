d0c1aed19507e9586387865f6cb7bc8f84ced509
dphuang2 <dylan@konfigthis.com>
2024-06-02 19:47:55 -0700

    better styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -49,7 +49,7 @@ export default function Home(): JSX.Element {
 function About() {
   return (
     <div>
-      <p className="lead">
+      <p>
         Until now, trying to style an article, document, or blog post with
         Tailwind has been a tedious task that required a keen eye for typography
         and a lot of complex custom CSS.
