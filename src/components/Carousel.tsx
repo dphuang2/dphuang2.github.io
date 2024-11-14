df90c982f6c6e3cfde30105911b527dc7d33a481
dphuang2 <dylan@konfigthis.com>
2024-11-13 19:15:12 -0800

    TODO: fix rendering of htmx blog post

---
+++ src/components/Carousel.tsx
@@ -0,0 +1,64 @@
+import React, { Children, PropsWithChildren, useEffect, useRef } from "react";
+
+function Wrapper({ children }: PropsWithChildren<{}>) {
+  useEffect(() => {
+    import("swiper/element/bundle").then((swiper) => {
+      swiper.register();
+      const els = document.querySelectorAll("swiper-container");
+      els.forEach((el) => {
+        Object.assign(el, {
+          injectStyles: [
+            `.swiper-button-prev, .swiper-button-next {
+  padding: 5px;
+}
+:host {
+  --swiper-navigation-size: 25px;
+}
+
+.swiper {
+  padding-bottom: 20px;
+}
+
+@media (max-width: 640px) {
+  :host {
+    --swiper-navigation-size: 0px;
+  }
+}
+`,
+          ],
+        });
+      });
+      els.forEach((el) => {
+        el.initialize();
+      });
+    });
+  }, []);
+  return (
+    <swiper-container
+      loop
+      navigation
+      pagination
+      pagination-clickable
+      init={false}
+    >
+      {Children.toArray(children)
+        .filter((c) => {
+          return React.isValidElement(c) && c.type === Slide;
+        })
+        .map((c, idx) => {
+          return <swiper-slide key={idx}>{c}</swiper-slide>;
+        })}
+    </swiper-container>
+  );
+}
+
+function Slide({ children }: PropsWithChildren<{}>) {
+  return children;
+}
+
+const Carousel = {
+  Wrapper,
+  Slide,
+};
+
+export default Carousel;
