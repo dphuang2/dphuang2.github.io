e3310c514c00dd3ac149838b60b4d41f71b06f59
dphuang2 <dylan@konfigthis.com>
2024-12-03 22:41:30 -0800

    fix comment

--- markdown-resume/generate-pdf.sh
+++ markdown-resume/generate-pdf.sh
@@ -2,7 +2,7 @@
 
 # Generate HTML first
 pandoc resume.md -f markdown -t html -c resume-stylesheet.css -s -o resume.html
-# Use weasyprint to convert HTML to PDF
+# Use puppeteer to convert HTML to PDF
 node generate-pdf.js
 # Copy resume.pdf to static directory
 cp resume.pdf ../static/
